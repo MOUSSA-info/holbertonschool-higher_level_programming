b247519f8f84dd020af325f9c046141b549233cf
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-16 00:05:42 +0200

    6 task

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    # TESTS ICI (INDENTÉS DE 4 ESPACES)
+    def test_ordered_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    # ... autres méthodes de test ...
+
