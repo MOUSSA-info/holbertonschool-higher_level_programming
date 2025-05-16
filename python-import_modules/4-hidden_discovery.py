051fbf2da7facaae4e7e541fc5b0f32fa7f5543b
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-16 11:11:04 +0000

    4 task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import hidden_4
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
+def main():
     names = dir(hidden_4)
     for name in names:
-        if name [:2] != "__":
+        if not name.startswith("__"):
             print(name)
+
+if __name__ == "__main__":
+    main()
