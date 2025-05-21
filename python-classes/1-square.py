ba6926b31f7edb87c533a5e617a341f3078dd928
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-21 14:16:13 +0200

    1 task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """Represents a square with a private size attribute."""
+    """Represents a square."""
 
     def __init__(self, size):
-        self.__size = size  # Private attribute using name mangling
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size  # Private instance attribute
