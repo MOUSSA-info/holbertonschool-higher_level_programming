3e7edf9bf80e3a5acfc554fdfdb31e3ef8fcc9b8
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-23 23:48:44 +0200

    6 task

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""Defines a Square class with size and position."""
 
 class Square:
+    """Represents a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        return(self.__size)
+        """Retrieve the size of the square."""
+        return self.__size
+
     @size.setter
     def size(self, value):
+        """Set the size of the square with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -18,22 +24,32 @@ class Square:
 
     @property
     def position(self):
-        return(self.__position)
+        """Retrieve the position of the square."""
+        return self.__position
+
     @position.setter
     def position(self, value):
-        if not (isinstance(value, tuple) or len(value) != 2
-        or not all (isinstance(v, int) and v >= 0 for v in value)):
+        """Set the position of the square with validation."""
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not isinstance(value[0], int) or
+            not isinstance(value[1], int) or
+            value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        return(self.__size ** 2)
+        """Return the area of the square."""
+        return self.__size ** 2
 
     def my_print(self):
+        """Print the square with the character #, considering position."""
         if self.__size == 0:
             print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print("_" * self.__position[0] + "#" * self.__size)
+            return
+        # Print newlines for position[1]
+        for _ in range(self.__position[1]):
+            print()
+        # Print each line of the square
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
