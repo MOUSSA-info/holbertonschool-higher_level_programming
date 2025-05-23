ee20b816ce2f1dbbd6cdcbc8a624434e6314baf5
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-23 17:31:01 +0200

    6 task

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Defines a Rectangle class with instance counting."""
 
-
 class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle and increment instance counter."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
