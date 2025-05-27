efac44dbd3dce7ae9d76a0cc554b6cdec8f07fd0
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-27 19:28:32 +0200

    6 task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class with an area method.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
