6beea3eb5b44d2ec75fc0d11644b234d4a0bfbed
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-05-27 19:26:33 +0200

    5 task

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a base class for geometry objects.
+
+BaseGeometry is intended to be extended by other geometry-related classes.
+Currently, it does not implement any attributes or methods.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry serves as a foundation for geometry classes.
+
+    This class is intentionally left empty and is meant to be subclassed
+    by other geometry-related classes in future implementations.
+    """
+    pass
