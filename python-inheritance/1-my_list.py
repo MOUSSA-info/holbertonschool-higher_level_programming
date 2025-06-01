4e8e654456f2a294d9339024ea538f4e2eed036a
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-06-01 14:47:31 +0200

    1 task

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-"""
-This module defines the MyList class.
-
-The purpose of this module is to provide a custom list class
-that inherits from the built-in list and adds a method to print
-the list sorted in ascending order.
-"""
+"""Defines a class MyList that inherits from list."""
 
 
 class MyList(list):
-    """
-    MyList is a subclass of list that provides a method to print
-    the list elements in ascending sorted order.
-    """
+    """A subclass of list that prints the list sorted."""
 
     def print_sorted(self):
-        """
-        Prints the list in ascending sorted order.
-
-        This method does not modify the original list.
-        """
+        """Prints the list in ascending sorted order."""
         print(sorted(self))
