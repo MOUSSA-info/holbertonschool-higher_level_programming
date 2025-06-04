520795f41bd1997285608b9f0b384cea9efd9313
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-06-04 13:33:40 +0200

    6 task

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import json
+
+def load_from_json_file(filename):
+    """Crée un objet Python à partir d'un fichier JSON."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
